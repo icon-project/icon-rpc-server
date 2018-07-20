dbc4825eae5683625993bbd45680df7f24ede09f
leeheonseung <mycom22@icon.Foundation>
2018-07-21 00:58:40 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ if [[ PYVER -ne 3 ]];then
 fi
 
 pip install -r requirements.txt
-wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/earlgrey-0.0.2-py3-none-any.whl"
-pip install --force-reinstall earlgrey-0.0.2-py3-none-any.whl
+WGET_VER=$(curl http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/earlgrey/VERSION)
+pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/earlgrey/earlgrey-${WGET_VER}-py3-none-any.whl"
 rm -rf earlgrey*
-wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconcommons-0.9.5-py3-none-any.whl"
-pip install --force-reinstall iconcommons-0.9.5-py3-none-any.whl
+
+WGET_VER=$(curl http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconcommons/VERSION)
+pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconcommons/iconcommons-${WGET_VER}-py3-none-any.whl"
 rm -rf iconcommons*
 
 
