5e95a91fb14829f6309d663c5004ee56759d35f8
Seungmin.Ryu <yakkle@gmail.com>
2021-09-07 18:20:58 +0900

    [IC2-317] fixed invalid channel name
     - work fine when path included trailing slash

--- iconrpcserver/dispatcher/v3d/version3d.py
+++ iconrpcserver/dispatcher/v3d/version3d.py
@@ -42,11 +42,10 @@ class Version3DebugDispatcher:
     It is used for accessing to only citizen node.
     """
     @staticmethod
-    async def dispatch(request: 'SanicRequest', channel_name=None):
+    async def dispatch(request: 'SanicRequest', channel_name: str = ""):
         req_json = request.json
         url = request.url
-        channel = (channel_name if channel_name is not None
-                   else StubCollection().conf[ConfigKey.CHANNEL])
+        channel = channel_name if channel_name else StubCollection().conf[ConfigKey.CHANNEL]
 
         context = {
             "url": url,
